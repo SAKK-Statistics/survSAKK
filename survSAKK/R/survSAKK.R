1fbe2df9c4cd8918529ef17a0fbd1f1343650fae
SAKK-vsomas <vithersan.somasundaram@sakk.ch>
2023-12-06 10:21:33 +0100

    added legend options

--- survSAKK/R/survSAKK.R
+++ survSAKK/R/survSAKK.R
@@ -24,38 +24,63 @@
 
 
 # adjustable parameters for the function
+
 fit <- survobject                                                               # fit:      An object of class `survfit`, usually returned by the `survfit` funciton. 
 col <- NULL                                                                     # col:      Can accept a single value for color, or a vector of color values to set color(s)
+# Layout options
+main <- NULL                                                                    # main:     Title
+sub <- NULL                                                                     # sub:      Subtitle
 xlab <- "Time"                                                                  # xlab:     Label given to x-axis
 ylab <- "Estiamted Survival Probability"                                        # ylab:     Label given to y-axis
 cex.lab <- 1                                                                    # cex.lab   A numeric value specifying the size of the xlab and ylab.
-lty <- "solid"                                                                  # lty:      A vector of integers specifying line types for each curve. The default value is 1.
-lwd <- "1"                                                                      # lwd:      A vector of numeric values for line widths. The default value is 1.
+cex.axis <- 0.75                                                                # cex.axis  A numeric values specifying the size of the axis size. 
+lty <- "solid"                                                                  # lty:      A vector of string specifying line types for each curve (“blank”, “solid”, “dashed”, “dotted”, “dotdash”, “longdash”, “twodash”).
+lwd <- 1                                                                        # lwd:      A vector of numeric values for line widths.
 xlim <- seq(from = 0, to = ceiling(max(fit$time))+ceiling(min(fit$time)))       # xlim:     Set xlim based on the range, seq(starting value,  end value, number of increment of the sequence)
 ylim <- seq(from = 0, to = 1, by = 0.25)                                        # ylim:     Set ylim based on the range, seq(starting value,  end value, number of increment of the sequence)
-
+# Legend options
+show.legend <- TRUE                                                             # show.legend        Display legend 
+legend.position <- "topright"                                                   # legend.position    Position of the legend, c(x,y), "bottomright", "bottom", "bottomleft", "left", "topleft", "top", "topright", "right" and "center".
+legend.legend <- NULL                                                           # legend.legend      A vector of string given to legend.
+legend.text.font <- 1                                                           # legend.text.font   An integer specifying the font style of the legend text; (1: normal, 2: bold, 3: italic, 4: bold and italic)
+legend.cex <- 0.75                                                                 # legend.cex         expansion factor for legend text.
+legend.title <- NULL                                                            # legend.title       The title of the legend
+legend.title.cex <- 1                                                           # legend.title.cex   expansion factor for legend title. 
 
 #- Function:
+
+# Falls Script zu lang wird sollten wir die Preparation auf einem seperaten R file abspeichern
+
+# Preparation
 
 # Extract data from fit
 data <- as.data.frame(eval(fit$call$data)) 
 
-# Colouring KM-Plot
+# Define color for KM-Plot if not manually specified
 if (is.null(col)){
   if(is.null(fit$strata)){
     col <- "black"
-    group <- ""
   } else {
     col <- as.factor(names(fit$strata))
-    group <- names(fit$strata)
   }
 } 
+
+# Extract Group names for legend if not manually specifed
+if(is.null(fit$strata)){
+  group <- "Cohort"
+  legend.legend <- group                                                          
+} else {
+  group <- levels(as.factor(names(fit$strata)))
+  legend.legend <- group
+}
 
 
 # KM-Plot
 base::plot(
   ## Plot the survival curve
   fit,
+  main = main,
+  sub = sub,
   col = col,                               
   lty = lty,
   lwd = lwd,
@@ -74,19 +99,37 @@ base::plot(
 # Customize the x coordinates
 graphics::axis(
   side = 1,                                # Specifies the side (1,2,3,4)
-     las = 0,                              # Rotate the labels
-     mgp = c(3,0.50,0),                    # Adjust the label position (axis title, axis label, axis line)
-     at = xlim,                            # Specify tick mark position
-     labels = xlim                         # Draw labels
+  las = 0,                                 # Rotate the labels
+  mgp = c(3,0.50,0),                       # Adjust the label position (axis title, axis label, axis line)
+  at = xlim,                               # Specify tick mark position
+  labels = xlim,                           # Draw labels
+  cex.axis = cex.axis                      # Axis size    
 )
 
 # Customize the y coordinates
-graphics::axis(side = 2,                             # Specifies the side (1,2,3,4)
+graphics::axis(side = 2,                   # Specifies the side (1,2,3,4)
      las = 1,                              # Rotate the labels 
      mgp = c(3,0.75,0),                    # Adjust the label position (axis title, axis label, axis line)
      at = ylim,                            # Specify tick mark position
-     labels = ylim                         # Draw labels
+     labels = ylim,                        # Draw labels
+     cex.axis = cex.axis                   # Axis size  
 )
+
+# Add legend to plot
+if (show.legend == TRUE){
+legend(x = legend.position[1],
+       y = legend.position[2],
+       legend = legend.legend ,
+       bty = "n",
+       col = col,
+       lty = lty,
+       text.font = legend.text.font,
+       title = legend.title,
+       cex = legend.cex,
+       title.cex = legend.title.cex
+       )
+}
+
 # Draw risk table
 # text(x = 0:xlim[2],                      # Starting point of the x values
 #      y = par("usr")[3] - 1,              # Starting point of the y values
@@ -116,7 +159,3 @@ survobject <- survival::survfit(Surv(time_yr, status) ~ sex, data = lung)
 survobject <- survival::survfit(Surv(time_mt, status) ~ 1, data = lung)
 
 survobject <- survival::survfit(Surv(time_yr, status) ~ 1, data = lung)
-
-surv.plot(S = fit)
-
-
