bb26e1c7b545f23456833a0b2b07c69e7cbc90ae
SAKK-vsomas <vithersan.somasundaram@sakk.ch>
2023-12-19 15:41:55 +0100

    adapted example due to check warnings

--- survSAKK/R/example.R
+++ survSAKK/R/example.R
@@ -10,25 +10,45 @@
 #' # Survival Object with two arms
 #'survobject1 <- survival::survfit(Surv(time_mt, status) ~ sex, data = lung)
 #'survobject2 <- survival::survfit(Surv(time_yr, status) ~ sex, data = lung)
-#' # Survival Obhect with one arm
+#'
+#' # Survival Object with one arm
 #'survobject3 <- survival::survfit(Surv(time_yr, status) ~ 1, data = lung)
 #'survobject4 <- survival::survfit(Surv(time_mt, status) ~ 1, data = lung)
 #'
-#'surv.plot(fit = survobject)
+#'
 #'survSAKK::surv.plot(fit = survobject1)
-#'survSAKK::surv.plot(fit = survobject2, xlim = seq(0,3), legend.legend = c("Female","Male"))
+#'
+#'survSAKK::surv.plot(fit = survobject2, xlim = seq(0,3),
+#'                    legend.legend = c("Female","Male"))
+#'
 #'survSAKK::surv.plot(fit = survobject3, xlim = seq(0,3))
+#'
 #'survSAKK::surv.plot(fit = survobject4)
-#'survSAKK::surv.plot(fit = survobject2, xlim = seq(0,3), segment.type = 3, segment.timepoint = 0.5, segment.text.position = "left")
-#'survSAKK::surv.plot(fit = survobject2, xlim = seq(0,3), segment.type = 3, segment.quantile = 0.5, segment.text.position = "bottomleft")
-#'survSAKK::surv.plot(fit = survobject2, xlim = seq(0,3), segment.type = 3, segment.quantile = 0.25, segment.text.position =c(0.35,0.4))
+#'
+#'survSAKK::surv.plot(fit = survobject2, xlim = seq(0,3),
+#'                    segment.type = 3,
+#'                    segment.timepoint = 0.5,
+#'                    segment.text.position = "left")
+#'survSAKK::surv.plot(fit = survobject2, xlim = seq(0,3),
+#'                    segment.type = 3,
+#'                    segment.quantile = 0.5,
+#'                    segment.text.position = "bottomleft")
+#'
+#'survSAKK::surv.plot(fit = survobject2, xlim = seq(0,3),
+#'                    segment.type = 3,
+#'                    segment.quantile = 0.25,
+#'                    segment.text.position =c(0.35,0.4))
+#'
 #'survSAKK::surv.plot(fit = survobject2, xlim = seq(0,3),
 #'                    col =c("pink","lightblue"),
 #'                    segment.type = 3,
 #'                    segment.quantile = 0.5,
 #'                    segment.text.position = "none")
 #'
-#'survSAKK::surv.plot(fit = survobject2, xlim = seq(0,3), segment.type = 3, segment.timepoint = 0.5, segment.text.position = "none")
+#'survSAKK::surv.plot(fit = survobject2, xlim = seq(0,3),
+#'                    segment.type = 3,
+#'                    segment.timepoint = 0.5,
+#'                    segment.text.position = "none")
 #'
 #'survSAKK::surv.plot(fit = survobject2,
 #'                    col =c("purple","green"),
