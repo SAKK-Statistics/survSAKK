d582f149e847ec62fb6676386c18067ad0947176
SAKK-vsomas <vithersan.somasundaram@sakk.ch>
2024-01-22 15:42:57 +0100

    risktable.pos description

--- R/surv.plot.R
+++ R/surv.plot.R
@@ -117,8 +117,7 @@
 #'    - `3` italic
 #'    - `4` bold-italic
 #' @param risktable A logical parameter indicating whether to draw risk table. Default: \code{TRUE}.
-#' @param risktable.pos Defines on which MARgin line the xlab is displayed. Starting at 0 counting outwards. Default at line 4.
-#'    should be drawn if `risktable` is drawn. Default: 2.5 line distances form the axis elements.
+#' @param risktable.pos Defines on which MARgin line the xlab is displayed. Starting at 0 counting outwards. Default at line 3.
 #' @param margin.bottom Specifies the bottom margin of the plotting area in line units.
 #' @param margin.left Specifies the left margin of the plotting area for in line units.
 #' @param risktable.title Title of risk table.
