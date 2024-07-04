5406343260db18aa2b7ea6ef6cf096a08afcf9de
SAKK-vsomas <vithersan.somasundaram@sakk.ch>
2024-07-04 13:44:50 +0200

    R Script Coverage

---
+++ R_dev/package_coverage.R
@@ -0,0 +1,13 @@
+library(covr)
+
+# Generate the coverage report
+report <- covr::report()
+
+# # Specify the directory where you want to save the report
+# save_path <- "C:/Users/vithersans/Desktop"
+#
+# # Combine the save path with the report filename
+# report_filename <- paste0(save_path, "coverage_report.html")
+#
+# # Save the report
+# covr::report(report, file = report_filename)
